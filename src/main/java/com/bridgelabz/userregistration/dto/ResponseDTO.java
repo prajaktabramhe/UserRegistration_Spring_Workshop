7023c1b423e827f5f95838c67146e382b4406d88
prajaktabramhe <prajakta14bramhe@gmail.com>
2021-05-26 20:03:54 +0530

    [Prajakta]: Added: User Registraion dto folder

---
+++ src/main/java/com/bridgelabz/userregistration/dto/ResponseDTO.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.userregistration.dto;
+
+import lombok.Data;
+
+public @Data class ResponseDTO {
+	private String message;
+	private Object data;
+	
+	
+	public ResponseDTO(String message, Object data) 
+	{
+		super();
+		this.message = message;
+		this.data = data;
+	}
+	
+}
